24ab0ee7977b5ceff0be8d05a572b67e8c8c2ee5
harshalmore31 <harshalmore2468@gmail.com>
2025-06-01 01:21:37 +0530

    Implement ProtectedRoute component for role-based access control and update routing in App.jsx

--- Frontend/src/lib/supabaseClient.js
+++ Frontend/src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://xoqqqmuoopodozxlfhls.supabase.co"; 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvcXFxbXVvb3BvZG96eGxmaGxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNzMzMTgsImV4cCI6MjA2Mzg0OTMxOH0.RN7UmDmAGu3OX4HEiMoLgk4A7L3tkxvVnVx_-wJtRH8"; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
